b0bb2f15df3f143ea9d0c38fc9faa54cfb5729da
Amulya Jain <amuly.jain@gmail.com>
2025-02-07 21:15:36 -0500

    Init

---
+++ src/main/java/prizehoggers/songmaker/SongMakerApplication.java
@@ -0,0 +1,14 @@
+package prizehoggers.songmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongMakerApplication.class, args);
+        System.out.println("Hello, world!");
+    }
+
+}
---
+++ src/test/java/prizehoggers/songmaker/SongMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package prizehoggers.songmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
