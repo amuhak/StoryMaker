aeefd33eb13eaaff43b27f734050f35e5ab1c271
Amulya Jain <amuly.jain@gmail.com>
2025-02-07 23:15:56 -0500

    Working Webserver

--- src/main/java/prizehoggers/songmaker/Controller/MakerController.java
+++ src/main/java/prizehoggers/songmaker/Controller/MakerController.java
@@ -1,11 +1,15 @@
 package prizehoggers.songmaker.Controller;
 
 import org.slf4j.Logger;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
-@EnableAsync
+@EnableWebSocketMessageBroker
 @Controller
 public class MakerController {
     public final Logger logger = org.slf4j.LoggerFactory.getLogger(MakerController.class);
@@ -15,4 +19,10 @@ public class MakerController {
         logger.info("User accessed the index page");
         return "index";
     }
+/*
+    @PostMapping("/input")
+    ResponseEntity<?> input(@RequestBody Webho webhookRequest) {
+
+    }
+*/
 }
--- src/main/java/prizehoggers/songmaker/SongMakerApplication.java
+++ src/main/java/prizehoggers/songmaker/SongMakerApplication.java
@@ -1,11 +1,22 @@
 package prizehoggers.songmaker;
 
+import org.apache.commons.text.RandomStringGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 @SpringBootApplication
 public class SongMakerApplication {
+    public static ConcurrentLinkedQueue<String> auth;
+    public static RandomStringGenerator generator;
+
     public static void main(String[] args) {
+        auth = new ConcurrentLinkedQueue<>();
+        generator =
+                new RandomStringGenerator.Builder().selectFrom(
+                        "abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray())
+                        .get();
         SpringApplication.run(SongMakerApplication.class, args);
     }
 }
