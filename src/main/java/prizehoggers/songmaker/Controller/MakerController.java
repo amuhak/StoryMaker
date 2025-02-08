09703c78b742af1ebb0ae8b5c7a54c4b8486dfd8
Amulya Jain <amuly.jain@gmail.com>
2025-02-08 01:26:12 -0500

    Working queue

--- src/main/java/prizehoggers/songmaker/Controller/MakerController.java
+++ src/main/java/prizehoggers/songmaker/Controller/MakerController.java
@@ -1,15 +1,9 @@
 package prizehoggers.songmaker.Controller;
 
 import org.slf4j.Logger;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
-@EnableWebSocketMessageBroker
 @Controller
 public class MakerController {
     public final Logger logger = org.slf4j.LoggerFactory.getLogger(MakerController.class);
@@ -19,10 +13,4 @@ public class MakerController {
         logger.info("User accessed the index page");
         return "index";
     }
-/*
-    @PostMapping("/input")
-    ResponseEntity<?> input(@RequestBody Webho webhookRequest) {
-
-    }
-*/
 }
